7a0e0867a57bbb9d4ac3d98dbfbeac7f764f0d33
Ali <work.gh.2021@gmail.com>
2024-02-07 23:33:05 +0330

    navbar (css - part 4)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,43 @@
 const navToggle = document.querySelector("#nav-toggle");
 const navMenu = document.querySelector("#nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
 
 // show menu - hide menu
 navToggle.addEventListener("click", function (){
     navMenu.classList.toggle("show-menu");
 });
+
+// add shadow to header
+function scrollHeader(){
+    const nav = document.getElementById("header")
+    if(this.scrollY >= 80) nav.classList.add("header-shadow"); else nav.classList.remove("header-shadow")
+}
+window.addEventListener('scroll', scrollHeader);
+
+// scroll section active link
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+// remove menu (when link is clicked)
+function linkAction() {
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
